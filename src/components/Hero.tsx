272981048f08a5023fcb179f8538c1fb5305b705
Nur Arini <nurarini0302@gmail.com>
2024-10-26 21:11:28 +0700

    [ADD] add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-[#aadcb6]'>
+      <div className='w-full sm:w-[46%] flex justify-center'>
+        <h1>Lorem, ipsum dolor.</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+      </div>
+      <div className='w-full sm:w-[46%]'>
+          <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex w-full justify-between items-center py-6 px-20'>
-      <div className='font-serif text-3xl font-bold'>
+    <nav className='flex w-full justify-between items-center py-6 px-32'>
+      <div className='font-jost text-3xl font-bold'>
         SAGA
       </div>
-      <ul className='flex gap-3'>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
+      <ul className='flex gap-5 text-[15px]'>
+        <li className='font-montserrat font-thin tracking-wider'>Home</li>
+        <li className='font-montserrat font-thin tracking-wider'>Products</li>
+        <li className='font-montserrat font-thin tracking-wider'>Contacts</li>
       </ul>
     </nav>
   )
