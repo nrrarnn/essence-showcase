76890d687cbd20255247bd79eb8e6718e20c496f
Nur Arini <nurarini0302@gmail.com>
2024-10-29 20:13:14 +0700

    [UP] up hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <div className='flex justify-center items-center h-[500px] bg-[#aadcb6]'>
       <div className='w-full sm:w-[46%] flex flex-col justify-center px-20'>
-        <h1 className='font-montserrat text-4xl font-bold'>Lorem, ipsum dolor.</h1>
+        <h1 className='font-montserrat text-4xl font-extrabold'>Lorem, ipsum dolor.</h1>
         <h3 className='font-montserrat text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
       </div>
       <div className='w-full sm:w-[46%]'>
-        <img src="/images/hero1.png" alt="hero image" className='w-[400px]' />
+        <img src="/images/p1.png" alt="hero image" className='w-[400px]' />
       </div>
     </div>
   )
